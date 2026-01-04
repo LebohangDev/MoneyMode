91766d8b370099f9d575d4b7e7fed8a8b83a7ef7
vk-varun <97547208+vk-varun@users.noreply.github.com>
2026-01-03 23:39:22 -0700

    Update routing

--- MoneyMode-Project/src/App.jsx
+++ MoneyMode-Project/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Nav from './Nav/Nav.jsx';
 import Home from './Home/Home.jsx';
 import About from './About/About.jsx';
+import Video from "./Video/Video.jsx";
+import StarterKit from "./Starter Kit/StarterKit.jsx";
 import Product from './Product/Product.jsx';
 import Testimonials from './Testimonials/Testimonials.jsx';
 import Gallery from './Gallery/Gallery.jsx';
@@ -15,6 +17,8 @@ function App() {
       <main>
         <Home />
         <About />
+        <Video />
+        <StarterKit />
         <Product />
         <Testimonials />
         <Gallery />
