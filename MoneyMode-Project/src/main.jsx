bf6f8094d317f00241a7b178a838c644833da673
LebohangDev <phxsoul39@gmail.com>
2026-02-03 14:43:11 +0400

    pushing fix

--- MoneyMode-Project/src/main.jsx
+++ MoneyMode-Project/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MoneyMode">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- MoneyMode-Project/vite.config.js
+++ MoneyMode-Project/vite.config.js
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/MoneyMode/' : '/',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
       },
-    },
+    }
   }
 })
